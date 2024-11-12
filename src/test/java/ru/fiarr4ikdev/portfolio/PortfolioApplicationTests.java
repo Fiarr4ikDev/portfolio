778adcc0d8c6e50e447e5a0e3db036c70e747153
Fiarr4ik <fiarr4ik@gmail.com>
2024-11-12 23:27:15 +0700

    init commit

---
+++ src/test/java/ru/fiarr4ikdev/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ikdev.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
